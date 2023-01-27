1c18c9c7c68923fb718e0b889304784d60672dca
JOAN CORTEZ LLORCA <joacorllo@alu.edu.gva.es>
2023-01-27 09:53:04 +0100

    Añadido módulo de ejemplo y  lista_tareas creado pero no funcional

--- addons/lista_tareas/__manifest__.py
+++ addons/lista_tareas/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "moduloPrueba",
+    'name': "Lista de tareas",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Sencilla Lista de tareas""",
 
     'description': """
-        Long description of module's purpose
+        Sencilla lista de tareas utilizadas para crear un nuevo módulo con un nuevo modelo de datos
     """,
 
-    'author': "My Company",
+    'author': "Joan Cortez",
     'website': "http://www.yourcompany.com",
+    'application':True,
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Productivity',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
