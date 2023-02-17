fc4af042c891cc10ea27bde9af552cb81f60b55d
JOAN CORTEZ LLORCA <joacorllo@alu.edu.gva.es>
2023-02-17 08:54:01 +0100

    Eliminado templates y demo de manifest

--- addons/lista_tareas/__manifest__.py
+++ addons/lista_tareas/__manifest__.py
@@ -26,10 +26,5 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ]
 }
